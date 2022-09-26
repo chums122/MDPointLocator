061f3b2a8f3dbaf88f785ad1cacbfc18bcca5646
Trevor Chumbley <chums122@yahoo.com>
2022-09-26 15:22:12 -0700

    Update PointCreateSubCommand.java

--- src/main/java/us/magicaldreams/mdpointlocator/commands/subcommands/point/PointCreateSubCommand.java
+++ src/main/java/us/magicaldreams/mdpointlocator/commands/subcommands/point/PointCreateSubCommand.java
@@ -3,10 +3,14 @@ package us.magicaldreams.mdpointlocator.commands.subcommands.point;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import us.magicaldreams.mdpointlocator.PointConfig;
 import us.magicaldreams.mdpointlocator.command.MDSubCommand;
 import us.magicaldreams.mdpointlocator.commands.PointBaseCommand;
 import us.magicaldreams.mdpointlocator.util.CommonUtil;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,7 +22,37 @@ public class PointCreateSubCommand implements MDSubCommand {
 
     @Override
     public void onCommand(CommandSender sender, Command command, String[] args) {
-        sender.sendMessage("works");
+
+        Player player = (Player) sender;
+        double scale = 1.0;
+
+        if (args.length < 3) {
+            player.sendMessage(CommonUtil.getBrandedMsgPrefix(ChatColor.RED + "Missing arguments! Please use " + getUsage()));
+            return;
+        } else if (args.length > 4) {
+            player.sendMessage(CommonUtil.getBrandedMsgPrefix(ChatColor.RED + "Too many arguments! Please use " + getUsage()));
+        } else if (!CommonUtil.isInteger(args[0]) || !CommonUtil.isInteger(args[1])) {
+            // If x and y are not integers
+            player.sendMessage(CommonUtil.getBrandedMsgPrefix(ChatColor.RED + "X and Z must be numbers!"));
+            return;
+        }
+
+        // Check if scale defined
+        if (CommonUtil.isNullArgument(args, 3)) {
+            if ((CommonUtil.isInteger(args[3])) || (CommonUtil.isDouble(args[3]))) scale = Double.parseDouble(args[3]);
+        } else {
+            player.sendMessage(CommonUtil.getBrandedMsgPrefix(ChatColor.RED + "Scale must be in the form of a number!"));
+            return;
+        }
+
+        if (!PointConfig.getConfig().contains(args[2])) {
+            player.sendMessage(CommonUtil.getBrandedMsgPrefix(ChatColor.AQUA + "The start point " + ChatColor.GREEN + args[2] + ChatColor.AQUA + " has been created with a scale of " + scale + ":1!"));
+            List<String> coords = Arrays.asList(args[0], args[1], String.valueOf(scale));
+            PointConfig.getConfig().set(args[2], coords);
+            PointConfig.saveConfig();
+        } else {
+            player.sendMessage(CommonUtil.getBrandedMsgPrefix(ChatColor.RED + "Start point already exists!"));
+        }
     }
 
     public String getPermission() {
@@ -26,7 +60,7 @@ public class PointCreateSubCommand implements MDSubCommand {
     }
 
     public String getUsage() {
-        return "/point create <x> <z> <name> <scale>";
+        return "/point create <x> <z> <name> (scale)";
     }
 
     public String getDescription() {
